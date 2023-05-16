8ee09b7be84cf48c0c37f82d712966b25976d661
ChanceKing <aaronchanceking17@gmail.com>
2023-05-16 17:59:48 -0400

    Base set up for combat screen

--- Cosmo-Truckers/Assets/Checked Out/Chance/Scripts/CombatManager.cs
+++ Cosmo-Truckers/Assets/Checked Out/Chance/Scripts/CombatManager.cs
@@ -7,8 +7,10 @@ using UnityEngine.UI;
 public class CombatManager : MonoBehaviour
 {
     [SerializeField] GameObject TempPage;
-    [SerializeField] TMP_Text Text;
-
+    [SerializeField] GameObject MiniGameScreen;
+    GameObject miniGame;
+    [SerializeField] TMP_Text Timer;
+ 
     [SerializeField] List<GameObject> EnemySelected;
 
     public void StartCombat(BaseAttackSO attack)
@@ -20,13 +22,13 @@ public class CombatManager : MonoBehaviour
             #region No Target
             case EnumManager.TargetingType.No_Target:
                 TempPage.SetActive(true);
-                Text.text = $"Doing Combat Stuff for {attack.AttackName}, no target. . .";
+                Debug.Log($"Doing Combat Stuff for {attack.AttackName}, no target. . .");
                 break;
             #endregion
             #region Self Target
             case EnumManager.TargetingType.Self_Target:
                 TempPage.SetActive(true);
-                Text.text = $"Doing Combat Stuff for {attack.AttackName}, self target. . .";
+                Debug.Log($"Doing Combat Stuff for {attack.AttackName}, self target. . .");
                 break;
             #endregion
             #region Single Target
@@ -40,7 +42,7 @@ public class CombatManager : MonoBehaviour
                         print(obj.gameObject.name);
                         EnemySelected.Add(obj.gameObject);
                         TempPage.SetActive(true);
-                        Text.text = $"Doing Combat Stuff for {attack.AttackName} against {EnemySelected[0].name}. . .";
+                        Debug.Log($"Doing Combat Stuff for {attack.AttackName} against {EnemySelected[0].name}. . .");
                     });
                 }
                 break;
@@ -48,7 +50,7 @@ public class CombatManager : MonoBehaviour
             #region Multi Target Cone
             case EnumManager.TargetingType.Multi_Target_Cone:
                 TempPage.SetActive(true);
-                Text.text = $"Doing Combat Stuff for {attack.AttackName}, Cone attack. . .";
+                Debug.Log($"Doing Combat Stuff for {attack.AttackName}, Cone attack. . .");
                 break;
             #endregion
             #region Multi Target Choice
@@ -65,12 +67,13 @@ public class CombatManager : MonoBehaviour
                         if (EnemySelected.Count == attack.numberOFTargets || EnemySelected.Count == FindObjectOfType<EnemyManager>().Enemies.Count)
                         {
                             TempPage.SetActive(true);
-                            Text.text = $"Doing Combat Stuff for {attack.AttackName} against";
+                            string text = $"Doing Combat Stuff for {attack.AttackName} against";
                             for(int i = 0; i < EnemySelected.Count; i++)
-                                Text.text += $" { EnemySelected[i].name } & ";
+                                text += $" { EnemySelected[i].name } & ";
 
-                            Text.text.Remove(Text.text.Length - 2, 2);
-                            Text.text += ". . .";
+                            text.Remove(text.Length - 2, 2);
+                            text += ". . .";
+                            Debug.Log(text);
                         }
                     });
                 }
@@ -79,7 +82,7 @@ public class CombatManager : MonoBehaviour
             #region AOE
             case EnumManager.TargetingType.AOE:
                 TempPage.SetActive(true);
-                Text.text = $"Doing Combat Stuff for {attack.AttackName}, AOE. . .";
+                Debug.Log($"Doing Combat Stuff for {attack.AttackName}, AOE. . .");
                 break;
             #endregion
             #region All Target
@@ -90,12 +93,13 @@ public class CombatManager : MonoBehaviour
                     if (EnemySelected.Count == FindObjectOfType<EnemyManager>().Enemies.Count)
                     {
                         TempPage.SetActive(true);
-                        Text.text = $"Doing Combat Stuff for {attack.AttackName} against";
+                        string text = $"Doing Combat Stuff for {attack.AttackName} against";
                         for (int i = 0; i < EnemySelected.Count; i++)
-                            Text.text += $" { EnemySelected[i].name } & ";
+                            text += $" { EnemySelected[i].name } & ";
 
-                        Text.text.Remove(Text.text.Length - 2, 2);
-                        Text.text += ". . .";
+                        text.Remove(text.Length - 2, 2);
+                        text += ". . .";
+                        Debug.Log(text);
                     }
                 }
                 break;
@@ -103,11 +107,33 @@ public class CombatManager : MonoBehaviour
 
             default: Debug.LogError($"{attack.targetingType} not set up."); break;
         }
+
+        StartCoroutine(StartMiniGame(attack));
+    }
+
+    IEnumerator StartMiniGame(BaseAttackSO attack)
+    {
+        float miniGameTime = 60.0f; //TODO add in time for each mini Game
+        Timer.text = miniGameTime.ToString();
+
+        miniGame = Instantiate(attack.CombatPrefab, MiniGameScreen.transform, false);
+        miniGame.transform.localScale = MiniGameScreen.transform.localScale;
+
+        while(miniGameTime >= 0)
+        {
+            miniGameTime -= Time.deltaTime;
+            Timer.text = ((int)miniGameTime).ToString();
+            yield return null;
+        }
+
+        EndCombat();
     }
 
     public void EndCombat()
     {
+        StopAllCoroutines();
         TempPage.SetActive(false);
+        Destroy(miniGame);
 
         foreach (var obj in FindObjectOfType<EnemyManager>().Enemies)
         {
@@ -115,5 +141,7 @@ public class CombatManager : MonoBehaviour
             button.interactable = false;
             button.onClick.RemoveAllListeners();
         }
+
+        FindObjectOfType<TurnOrder>().EndTurn();
     }
 }
