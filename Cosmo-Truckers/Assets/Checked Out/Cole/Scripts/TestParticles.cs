ab20b5857c2d0f12858c96f290d9a94ddb68274a
Cole <coleamilleruww@gmail.com>
2023-05-31 15:21:00 -0400

    Fan Prefab Updates

--- Cosmo-Truckers/Assets/Checked Out/Cole/Scripts/TestParticles.cs
+++ Cosmo-Truckers/Assets/Checked Out/Cole/Scripts/TestParticles.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class TestParticles : MonoBehaviour
 {
-    public ParticleSystem[] testParticles; 
+    public ParticleSystem[] parentParticles;
+
+    List <ParticleSystem> allParticles = new List <ParticleSystem>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetAllParticles();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool particlesEnabled = testParticles[0].emission.enabled;
-
-
         if (Input.GetKeyDown(KeyCode.P))
         {
-            foreach (var particle in testParticles)
+            foreach (var particle in allParticles)
             {
                 var emission = particle.emission;
 
@@ -32,6 +32,28 @@ public class TestParticles : MonoBehaviour
                 }
             }
         }
+    }
 
+    private void GetAllParticles()
+    {
+        foreach (var particle in parentParticles)
+        {
+            //Add parent particle
+            allParticles.Add(particle);
+
+            //Get count of all of its children
+            int children = particle.transform.childCount;
+
+            //Loop through its children, add them to list
+            for (int i = 0; i < children; i++)
+            {
+                Transform _child = particle.transform.GetChild(i);
+
+                if (_child.GetComponent<ParticleSystem>() != null)
+                {
+                    allParticles.Add(_child.GetComponent<ParticleSystem>());
+                }
+            }
+        }
     }
 }
