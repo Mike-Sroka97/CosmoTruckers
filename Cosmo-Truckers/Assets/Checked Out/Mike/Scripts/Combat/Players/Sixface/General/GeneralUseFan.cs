a935a4b6fe72e0119c63cce19f633c81d70293f3
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2023-07-19 16:31:58 -0400

    fuck you cole

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Sixface/General/GeneralUseFan.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Sixface/General/GeneralUseFan.cs
@@ -7,22 +7,24 @@ public class GeneralUseFan : MonoBehaviour
     [SerializeField] float blowForce;
     [SerializeField] float hoverBlowForce;
 
-    SixfaceINA sixFace;
+    SixfaceINA six;
+    PlayerBody sixFace;
     Rigidbody2D sixFaceBody;
     private void Start()
     {
         if(FindObjectOfType<SixfaceINA>())
         {
-            sixFace = FindObjectOfType<SixfaceINA>();
-            sixFaceBody = sixFace.GetComponent<Rigidbody2D>();
+            six = FindObjectOfType<SixfaceINA>();
+            sixFace = six.GetComponentInChildren<PlayerBody>();
+            sixFaceBody = six.GetComponent<Rigidbody2D>();
         }
-    }
+    } 
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (sixFace != null && collision.gameObject == sixFace.gameObject)
         {
-            if(sixFace.IsHovering)
+            if(six.IsHovering)
             {
                 sixFaceBody.velocity = new Vector2(sixFaceBody.velocity.x, sixFaceBody.velocity.y + hoverBlowForce);
             }
