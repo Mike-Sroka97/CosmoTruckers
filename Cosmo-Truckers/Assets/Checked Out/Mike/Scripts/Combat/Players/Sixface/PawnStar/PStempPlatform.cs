bb2dea61c07533b7ea390bbf60b81af70d27bf9d
Cole <coleamilleruww@gmail.com>
2023-07-20 11:32:16 -0400

    material updates, organization, QD and PS updates

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Sixface/PawnStar/PStempPlatform.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Sixface/PawnStar/PStempPlatform.cs
@@ -6,6 +6,8 @@ public class PStempPlatform : MonoBehaviour
 {
     SixfaceINA sixFace;
 
+    float timeToWait = 0.5f, timer; 
+
     private void Start()
     {
         sixFace = FindObjectOfType<SixfaceINA>();
@@ -13,9 +15,17 @@ public class PStempPlatform : MonoBehaviour
 
     private void Update()
     {
-        if(!sixFace.Grounded())
+        //Cole changed this code to wait 0.5 seconds
+        if (timer < timeToWait)
         {
-            Destroy(gameObject);
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            if (!sixFace.Grounded())
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
