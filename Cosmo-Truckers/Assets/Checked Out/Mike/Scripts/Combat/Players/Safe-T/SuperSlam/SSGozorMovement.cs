61b96527d95c5bb5b6e6c4ecbecb565e3ab49014
Cole <coleamilleruww@gmail.com>
2023-08-17 08:39:22 -0400

    Lmao, lol even. Haha funny

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Safe-T/SuperSlam/SSGozorMovement.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Safe-T/SuperSlam/SSGozorMovement.cs
@@ -39,6 +39,7 @@ public class SSGozorMovement : MonoBehaviour
         {
             minigameStarted = true;
             moveSpeed = originalMoveSpeed;
+            Debug.Log("MoveSpeed: " + moveSpeed + " OG: " + originalMoveSpeed);
         }
     }
 
