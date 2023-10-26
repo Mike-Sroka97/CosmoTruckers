192ebb19e0dc786e1bdba00c98be3e8dc2b3a403
Cole <coleamilleruww@gmail.com>
2023-10-26 08:52:58 -0400

    Organization, testfiles, RB updates

---
+++ Cosmo-Truckers/Assets/Checked Out/Cole/Scripts/Testing/TestTimer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class TestTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float maxTime;
+    private float currentTime;
+    public Image blackImage; 
+
+    private void Start()
+    {
+        currentTime = maxTime;
+        blackImage.enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime; 
+        int time = (int)currentTime;
+        timerText.text = time.ToString();
+
+        if (currentTime <= 0)
+        {
+            blackImage.enabled = true;
+        }
+    }
+}
