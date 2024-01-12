5dd4e61026d372f109071f63370ff0b3328a11d2
Cole <coleamilleruww@gmail.com>
2024-01-11 20:44:50 -0500

    LDG HR Temp Art & Updates

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Long Dog/LDGAttacks/HeadRush/CatGun.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Players/Long Dog/LDGAttacks/HeadRush/CatGun.cs
@@ -8,11 +8,18 @@ public class CatGun : MonoBehaviour
     [SerializeField] float fireCD;
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject projectileSpawn;
+    [SerializeField] Sprite fireMouth;
+    [SerializeField] float spriteWaitTime = 1f;   
+
+    SpriteRenderer myRenderer;
+    Sprite startingSprite; 
 
     float currentTime = 0;
 
     private void Start()
     {
+        myRenderer = GetComponent<SpriteRenderer>();
+        startingSprite = myRenderer.sprite; 
         float randomOffset = Random.Range(0.0f, fireOffset);
         currentTime += randomOffset;
     }
@@ -21,8 +28,17 @@ public class CatGun : MonoBehaviour
         if(currentTime >= fireCD)
         {
             Instantiate(projectile, projectileSpawn.transform);
+            StartCoroutine(SwitchSprite()); 
             currentTime = 0;
+
         }
         currentTime += Time.deltaTime;
     }
+
+    IEnumerator SwitchSprite()
+    {
+        myRenderer.sprite = fireMouth;
+        yield return new WaitForSeconds(spriteWaitTime);
+        myRenderer.sprite = startingSprite; 
+    }  
 }
