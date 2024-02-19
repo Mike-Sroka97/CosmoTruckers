1386deff05ac4478ee8a0e70ca9514c813a12dd5
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-02-18 21:17:49 -0500

    Mysterious Cane

---
+++ Cosmo-Truckers/Assets/Checked Out/Chance_Mike/AUGS/Scripts/GloomGloomGalaxy/Nodes/MysteriousCane/MysteriousCane.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteriousCane : Augment
+{
+    public override void Activate(DebuffStackSO stack = null)
+    {
+        if (!firstGo)
+        {
+            AugmentSO.MyCharacter.AdjustAttackDamage(-(int)StatusEffect);
+            AugmentSO.MyCharacter.AdjustSpeed(-(int)StatusEffect);
+        }
+
+        if (firstGo)
+            firstGo = false;
+
+        base.Activate(stack);
+
+        AugmentSO.MyCharacter.AdjustAttackDamage((int)StatusEffect);
+        AugmentSO.MyCharacter.AdjustSpeed((int)StatusEffect);
+    }
+    public override void StopEffect()
+    {
+        AugmentSO.MyCharacter.AdjustAttackDamage(-(int)StatusEffect);
+        AugmentSO.MyCharacter.AdjustSpeed(-(int)StatusEffect);
+    }
+}
