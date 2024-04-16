fd10a4d1e0bb9fe7355b172953a4e389c4156768
ChanceKing <aaronchanceking17@gmail.com>
2024-04-16 12:03:49 -0400

    Finished wishing star NC node

--- Cosmo-Truckers/Assets/Checked Out/Chance/Scripts/StaticScripts/MathCC.cs
+++ Cosmo-Truckers/Assets/Checked Out/Chance/Scripts/StaticScripts/MathCC.cs
@@ -45,4 +45,20 @@ public static class MathCC
             list[n] = value;
         }
     }
+
+    /// <summary>
+    /// Randomize Array
+    /// </summary>
+    public static void Shuffle<T>(this T[] list)
+    {
+        int n = list.Length;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 }
