82760291af0166bfa204bbd298aeb8ad5d57c59b
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-04-24 20:11:58 -0400

    emoji

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Emote/Emote.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Emote/Emote.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Emote : MonoBehaviour
 {
+    public GameObject EmoteToSpawn;
+    public Image Icon;
 
+    public void InitEmote(Emote emoteToModel)
+    {
+        if (!Icon)
+            Icon = GetComponentInChildren<Image>();
+
+        EmoteToSpawn = emoteToModel.EmoteToSpawn;
+        Icon.sprite = emoteToModel.Icon.sprite;
+    }
 }
