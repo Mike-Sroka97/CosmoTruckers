d6033c8f88e29c9a835fa33d9399e980421f6199
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-05-23 21:54:25 -0400

    GORP

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Cutscene/TutorialCutscenes/TutorialHUBCutscene.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Cutscene/TutorialCutscenes/TutorialHUBCutscene.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TutorialHUBCutscene : CutsceneController
 {
+    [SerializeField] GameObject gorpToEnable;
+    [SerializeField] GameObject roy;
     
     Animator eyeToActivate;
     [HideInInspector] public bool EyeStuffHappening;
@@ -16,6 +18,38 @@ public class TutorialHUBCutscene : CutsceneController
 
         while (EyeStuffHappening)
             yield return null;
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine(cameraController.FadeVignette(false));
+        while (cameraController.CommandsExecuting > 0)
+            yield return null;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        gorpToEnable.SetActive(true);
+
+        StartCoroutine(cameraController.FadeVignette(true));
+        while (cameraController.CommandsExecuting > 0)
+            yield return null;
+
+        yield return new WaitForSeconds(5f);
+
+        StartCoroutine(cameraController.FadeVignette(false));
+
+        while (cameraController.CommandsExecuting > 0)
+            yield return null;
+
+        yield return new WaitForSeconds(3f);
+
+        gorpToEnable.SetActive(false);
+        roy.SetActive(true);
+
+        StartCoroutine(cameraController.FadeVignette(true));
+
+        while (cameraController.CommandsExecuting > 0)
+            yield return null;
+
+        End();
     }
 
     public void EyeDone()
