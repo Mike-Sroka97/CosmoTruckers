20ba6f22b892450e80047f3432b2ac3540b607ec
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-05-27 10:47:56 -0400

    main menu navigation

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/HUB/HUBController.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/HUB/HUBController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class HUBController : MonoBehaviour
 {
+    [SerializeField] GameObject mainOptionsGO;
+    [SerializeField] GameObject dimensionVoteGO;
+    [SerializeField] GameObject characterSwapGO;
+    [SerializeField] GameObject spellCraftingGO;
+    [SerializeField] GameObject trainingGO;
+    [SerializeField] GameObject emotesGO;
+    [SerializeField] GameObject dataLogGO;
+
     int playersLockedIn;
 
     public void DisableAll()
@@ -15,5 +23,89 @@ public class HUBController : MonoBehaviour
 
 
         gameObject.SetActive(false);
+    }
+
+    public void DimensionVote(bool open)
+    {
+        if (open)
+        {
+            dimensionVoteGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            dimensionVoteGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
+    }
+
+    public void CharacterSwap(bool open)
+    {
+        if (open)
+        {
+            characterSwapGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            characterSwapGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
+    }
+
+    public void SpellCrafting(bool open)
+    {
+        if (open)
+        {
+            spellCraftingGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            spellCraftingGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
+    }
+
+    public void Training(bool open)
+    {
+        if (open)
+        {
+            trainingGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            trainingGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
+    }
+
+    public void Emotes(bool open)
+    {
+        if (open)
+        {
+            emotesGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            emotesGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
+    }
+
+    public void DataLog(bool open)
+    {
+        if (open)
+        {
+            dataLogGO.SetActive(true);
+            mainOptionsGO.SetActive(false);
+        }
+        else
+        {
+            dataLogGO.SetActive(false);
+            mainOptionsGO.SetActive(true);
+        }
     }
 }
