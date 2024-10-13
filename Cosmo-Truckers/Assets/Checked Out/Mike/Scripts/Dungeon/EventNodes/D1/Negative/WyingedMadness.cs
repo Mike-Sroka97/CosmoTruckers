33640368024a3dfb2b5dcc5ea14377b01baafc29
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-12 20:48:03 -0400

    wyinged madness

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/WyingedMadness.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/WyingedMadness.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WyingedMadness : EventNodeBase
 {
+    protected override void Start()
+    {
+        base.Start();
+        for(int i = 0; i < myButtons.Length; i++)
+            myButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = $"<color=red>Give {PlayerVesselManager.Instance.PlayerVessels[i].MyCharacter.CharacterName} (1) Nitemare";
+    }
 
+    public void GiveMadness(int buttonID)
+    {
+        PlayerVesselManager.Instance.PlayerVessels[buttonID].MyCharacter.AddDebuffStack(augmentsToAdd[0]);
+        IgnoreOption();
+    }
 }
