585635da4d6d5407b2f3e90e856941fde430bfd5
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-12 21:12:15 -0400

    spunky monkey

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/StickyGloomGuy.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/StickyGloomGuy.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class StickyGloomGuy : EventNodeBase
 {
+    [SerializeField] int dissolveDamage = 15;
 
+    public override void Initialize(EventNodeHandler handler)
+    {
+        base.Initialize(handler);
+        CheckDissolveStatus();
+    }
+
+    public void SwallowTheGloop()
+    {
+        currentCharacter.AddDebuffStack(augmentsToAdd[0]);
+        IteratePlayerReference();
+        CheckDissolveStatus();
+        AutoSelectNextButton();
+        CheckEndEvent();
+    }
+
+    public void Dissolve()
+    {
+        currentCharacter.TakeDamage(dissolveDamage);
+        IteratePlayerReference();
+        CheckDissolveStatus();
+        AutoSelectNextButton();
+        CheckEndEvent();
+    }
+
+    private void CheckDissolveStatus()
+    {
+        if (currentCharacter.CurrentHealth <= dissolveDamage)
+            myButtons[1].enabled = false;
+        else
+            myButtons[1].enabled = true;
+    }
 }
