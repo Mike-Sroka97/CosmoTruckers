23959e8ec9a23bef83689644faf660056807fc64
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-12 20:57:43 -0400

    nn nes

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/NoisyNuissance.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Negative/NoisyNuissance.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class NoisyNuissance : EventNodeBase
 {
+    public void ListenToTimmy()
+    {
+        currentCharacter.TakeHealing(5);
+        currentCharacter.AddDebuffStack(augmentsToAdd[0], 2);
+        IgnoreOption();
+    }
 
+    public void EscapeTimmy()
+    {
+        currentCharacter.AddDebuffStack(augmentsToAdd[0]);
+        IgnoreOption();
+    }
 }
