1795d061e356b422e25400448b3b12833a645f95
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-15 22:12:05 -0400

    i didnt test this code

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Positive/DepressedWalkingNoise.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Positive/DepressedWalkingNoise.cs
@@ -1,8 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DepressedWalkingNoise : EventNodeBase
 {
+    [SerializeField] float waitTime = .2f;
+    [SerializeField] string failureText;
+    [SerializeField] TextMeshProUGUI descriptionText;
+    [SerializeField] string description;
 
+    TextMeshProUGUI buttonText;
+    int timmysNumber = 1;
+    int lastNumber;
+
+    protected override void Start()
+    {
+        base.Start();
+        buttonText = myButtons[0].GetComponentInChildren<TextMeshProUGUI>();
+        timmysNumber = Random.Range(1, 11); //1-10
+        descriptionText.text = $"{description} <color=green>{timmysNumber}</color>.";
+        buttonText.text = timmysNumber.ToString();
+        StartCoroutine(ChangeNumber());
+    }
+
+    public void GuessNumber()
+    { 
+        if(lastNumber == timmysNumber)
+        {
+            StopAllCoroutines();
+            currentCharacter.RemoveAmountOfAugments(0, 999);
+            buttonText.text = $"<color=green>Timmy fully cleanses {currentCharacter.CharacterName}";
+            StartCoroutine(SelectionChosen());
+        }
+        else
+        {
+            StopAllCoroutines();
+            IgnoreOption();
+        }
+    }
+
+    protected override void CheckEndEvent()
+    {
+        currentTurns++;
+
+        if (currentTurns > 3)
+        {
+            buttonText.text = failureText;
+            StartCoroutine(SelectionChosen());
+        }
+    }
+
+    IEnumerator ChangeNumber()
+    {
+        //update number
+        buttonText.text = lastNumber.ToString();
+        int textNumber = int.Parse(buttonText.text);
+        textNumber++;
+        if (textNumber > 10)
+            textNumber = 1;
+
+        lastNumber = textNumber;
+        //set text
+        if (textNumber == timmysNumber)
+            buttonText.text = $"<color=green>{textNumber}";
+        else
+            buttonText.text = textNumber.ToString();
+
+        //reset
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(ChangeNumber());
+    }
 }
