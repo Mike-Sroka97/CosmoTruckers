1a7cd619d8eea69533043527ef3c4c068a6a8ca3
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-16 19:35:09 -0400

    shlorp

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/NovicExpansion.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/NovicExpansion.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class NovicExpansion : EventNodeBase
 {
-
+    public void ExplosiveExpansion()
+    {
+        currentCharacter.ProliferateAugment(3, 1);
+        currentCharacter.ProliferateAugment(3, 0);
+        IgnoreOption();
+    }
 }
