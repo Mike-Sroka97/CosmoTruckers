89b6868f8834e8b59fd5b01bb24552f247523759
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-17 20:18:24 -0400

    warp vortex

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/WarpVortex.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/WarpVortex.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class WarpVortex : EventNodeBase
 {
+    public void AcceptFate()
+    {
+        //Get healths and shuffle
+        List<int> currentHealth = new List<int>();
 
+        foreach(PlayerVessel vessel in PlayerVesselManager.Instance.PlayerVessels)
+            currentHealth.Add(vessel.MyCharacter.CurrentHealth);
+
+        MathHelpers.Shuffle(currentHealth);
+
+        //heal or damage when swapping with pierce
+        for(int i = 0; i < 4; i++)
+        {
+            PlayerCharacter currentCharacter = PlayerVesselManager.Instance.PlayerVessels[i].MyCharacter;
+
+            if (currentCharacter.CurrentHealth > currentHealth[i])
+                currentCharacter.TakeDamage(currentCharacter.CurrentHealth - currentHealth[i], true);
+            else if(currentCharacter.CurrentHealth < currentHealth[i])
+                currentCharacter.TakeHealing(currentHealth[i] - currentCharacter.CurrentHealth, true);
+        }
+
+        StartCoroutine(SelectionChosen());
+    }
 }
