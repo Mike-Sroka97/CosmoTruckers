f59455a4723705d7a097c6311667c4556a5bc5fa
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-18 20:49:35 -0400

    fds

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/FrightenedDwarfStar.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/FrightenedDwarfStar.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class FrightenedDwarfStar : EventNodeBase
 {
+    [SerializeField] int damage = 12;
 
+    bool evilStar;
+
+    protected override void Start()
+    {
+        base.Start();
+        evilStar = MathHelpers.RandomBool();
+        if (currentCharacter.CurrentHealth <= damage)
+            myButtons[0].enabled = false;
+        else
+            myButtons[0].enabled = true;
+
+    }
+
+    public void HelpTheStar()
+    {
+        if (evilStar)
+            currentCharacter.TakeDamage(damage);
+        else
+            currentCharacter.DoubleAugment(1);
+
+        IteratePlayerReference();
+        StartCoroutine(SelectionChosen());
+    }
+
+    public override void IgnoreOption()
+    {
+        IteratePlayerReference();
+        if (currentCharacter.CurrentHealth <= damage)
+            myButtons[0].enabled = false;
+        else
+            myButtons[0].enabled = true;
+        AutoSelectNextButton();
+        CheckEndEvent();
+    }
 }
