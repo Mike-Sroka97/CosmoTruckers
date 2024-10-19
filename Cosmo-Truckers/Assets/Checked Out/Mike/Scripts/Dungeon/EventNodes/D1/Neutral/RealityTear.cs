c359505835ec02567dedf03639c19da54f424ea3
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-18 21:11:09 -0400

    reality tear

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/RealityTear.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/RealityTear.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class RealityTear : EventNodeBase
 {
+    public void SwapAll()
+    {
+        //swap positions
+        PlayerVessel[] players = PlayerVesselManager.Instance.PlayerVessels;
+        MathHelpers.Shuffle(players);
 
+        for (int i = 0; i < players.Length; i++)
+            players[i].MyCharacter.FlipCharacter(i, true);
+
+        StartCoroutine(SelectionChosen());
+        IteratePlayerReference();
+    }
 }
