37b511f4aa8592c832fa1abce28d3a2b76ac067e
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-19 10:58:27 -0400

    bubble hat

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/MagicHat.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/MagicHat.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MagicHat : EventNodeBase
 {
+    int random;
+    string color;
 
+    protected override void Start()
+    {
+        base.Start();
+        random = Random.Range(0, 3);
+        if (random == 2)
+            color = "<color=red>";
+        else
+            color = "<color=green>";
+    }
+
+    public void ReachInside()
+    {
+        AddAugmentToPlayer(augmentsToAdd[random]);
+        AddAugmentToPlayer(augmentsToAdd[random]);
+        myButtons[0].GetComponentInChildren<TextMeshProUGUI>().text = $"{color}Gained (1) {augmentsToAdd[random].DebuffName}";
+        IteratePlayerReference();
+        currentTurns = 4;
+        CheckEndEvent();
+    }
 }
