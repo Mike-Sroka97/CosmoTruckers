4a81291510af950b311fdd2fe493156896b0dfd5
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-19 20:20:35 -0400

    return unto ash (i tested this)

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/ReturnUntoAsh.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/ReturnUntoAsh.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ReturnUntoAsh : EventNodeBase
 {
+    [SerializeField] float waitTime = 1f;
 
+    public void Rebirth()
+    {
+        currentCharacter.TakeDamage(999, true);
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        currentCharacter.Resurrect(currentCharacter.Health / 2, true);
+        IteratePlayerReference();
+        StartCoroutine(SelectionChosen());
+    }
 }
