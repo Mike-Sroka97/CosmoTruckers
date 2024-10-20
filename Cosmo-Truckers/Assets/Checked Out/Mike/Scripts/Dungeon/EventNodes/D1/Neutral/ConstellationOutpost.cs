c91821c68da045c138967f58076d7688313b8c49
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2024-10-19 21:11:10 -0400

    ben shapiro

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/ConstellationOutpost.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Dungeon/EventNodes/D1/Neutral/ConstellationOutpost.cs
@@ -1,8 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConstellationOutpost : EventNodeBase
 {
+    [SerializeField] int minSafeChance;
+    [SerializeField] int maxSafeChance;
+    [SerializeField] AugmentStackSO[] victoryAugments;
+    [SerializeField] AugmentStackSO[] failureAugments;
+    [SerializeField] int riskyAmount = 3;
 
+    int safeChance;
+    int riskyChance;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        safeChance = Random.Range(minSafeChance, maxSafeChance + 1);
+        riskyChance = 100 - safeChance; //difference from 100%
+
+        myButtons[0].GetComponentInChildren<TextMeshProUGUI>().text = $"Safe path ({safeChance}%)";
+        myButtons[1].GetComponentInChildren<TextMeshProUGUI>().text = $"Risky path ({riskyChance}%)";
+    }
+
+    public void SafeBet()
+    {
+        int random = Random.Range(1, 101);
+        
+        //success
+        if(random <= safeChance)
+            foreach (PlayerVessel player in PlayerVesselManager.Instance.PlayerVessels)
+                player.MyCharacter.AddAugmentStack(victoryAugments[Random.Range(0, victoryAugments.Length)]);
+        //failure
+        else
+            foreach (PlayerVessel player in PlayerVesselManager.Instance.PlayerVessels)
+                player.MyCharacter.AddAugmentStack(failureAugments[Random.Range(0, victoryAugments.Length)]);
+
+        IteratePlayerReference();
+        StartCoroutine(SelectionChosen());
+    }
+
+    public void RiskyBet()
+    {
+        int random = Random.Range(1, 101);
+
+        //success
+        if (random <= riskyChance)
+            foreach (PlayerVessel player in PlayerVesselManager.Instance.PlayerVessels)
+                player.MyCharacter.AddAugmentStack(victoryAugments[Random.Range(0, victoryAugments.Length)], riskyAmount);
+        //failure
+        else
+            foreach (PlayerVessel player in PlayerVesselManager.Instance.PlayerVessels)
+                player.MyCharacter.AddAugmentStack(failureAugments[Random.Range(0, victoryAugments.Length)], riskyAmount);
+
+        IteratePlayerReference();
+        StartCoroutine(SelectionChosen());
+    }
 }
