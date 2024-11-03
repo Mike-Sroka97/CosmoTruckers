9171eb06c5c048a4a332c0aa3341cedfb5796a25
Cole <coleamilleruww@gmail.com>
2024-11-03 09:13:22 -0500

    Orbital Drone Strike

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Normal/CosmicCrust/OrbitalCrust/OrbitalCrustStalactite.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Normal/CosmicCrust/OrbitalCrust/OrbitalCrustStalactite.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class OrbitalCrustStalactite : MonoBehaviour
 {
     [SerializeField] bool ground = false;
+    [SerializeField] Material noHurtMaterial;
+    [SerializeField] GameObject deathParticle;
+    float groundLayer = 9;
+    float minHeight = -1.3f; 
 
     Rigidbody2D parentBody;
     PolygonCollider2D parentCollider;
+    TrackPlayerDeath playerDeath;
+    SpriteRenderer sprite; 
 
     private void Start()
     {
@@ -15,6 +21,8 @@ public class OrbitalCrustStalactite : MonoBehaviour
         {
             parentBody = GetComponentInParent<Rigidbody2D>();
             parentCollider = GetComponentInParent<PolygonCollider2D>();
+            playerDeath = GetComponent<TrackPlayerDeath>();
+            sprite = transform.parent.GetComponentInChildren<SpriteRenderer>();
             Invoke("EnableParentCollider", 0.5f);
         }
     }
@@ -26,11 +34,24 @@ public class OrbitalCrustStalactite : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<OrbitalCrustStalactite>() && !ground)
+        if (!ground && transform.parent.localPosition.y < minHeight)
         {
-            parentBody.bodyType = RigidbodyType2D.Static;
-            parentBody.velocity = Vector2.zero;
-            Destroy(parentBody);
+            if (collision.GetComponent<OrbitalCrustStalactite>() || collision.gameObject.layer == groundLayer)
+            {
+                ground = true;
+                GetComponent<Collider2D>().enabled = false;
+                transform.parent.GetComponent<Collider2D>().enabled = true;
+                Destroy(playerDeath); 
+                sprite.material = noHurtMaterial;
+                parentBody.bodyType = RigidbodyType2D.Static;
+                parentBody.velocity = Vector2.zero;
+                Destroy(parentBody);
+            }
+            if (collision.tag == "Player")
+            {
+                Instantiate(deathParticle, transform.position, Quaternion.identity, FindObjectOfType<CombatMove>().transform);
+                Destroy(transform.parent.gameObject); 
+            }
         }
     }
 }
