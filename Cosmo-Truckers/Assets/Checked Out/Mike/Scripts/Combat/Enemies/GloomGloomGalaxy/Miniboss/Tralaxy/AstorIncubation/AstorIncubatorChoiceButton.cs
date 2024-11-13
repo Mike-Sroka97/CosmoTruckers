659ee8ee71b2249b9964e31bd5646b40aa458bed
Cole <coleamilleruww@gmail.com>
2024-11-12 21:11:09 -0500

    Waga Baga Bobo

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Miniboss/Tralaxy/AstorIncubation/AstorIncubatorChoiceButton.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Miniboss/Tralaxy/AstorIncubation/AstorIncubatorChoiceButton.cs
@@ -6,27 +6,46 @@ public class AstorIncubatorChoiceButton : MonoBehaviour
 {
     [SerializeField] int scoreValue;
     [SerializeField] int ballNumber;
+    [SerializeField] Material toggleMaterial;
+    [SerializeField] Sprite toggledSprite; 
 
-    AstorIncubation minigame; 
+    AstorIncubation minigame;
+    SpriteRenderer myRenderer;
+    Material defaultMaterial;
+    Sprite defaultSprite; 
 
     private void Start()
     {
-        minigame = transform.parent.parent.parent.GetComponent<AstorIncubation>();
+        minigame = FindObjectOfType<AstorIncubation>();
+        myRenderer= GetComponent<SpriteRenderer>();
+
+        defaultMaterial = myRenderer.material;
+        defaultSprite = myRenderer.sprite;
     }
 
     private void Update()
     {
         if (minigame.platformsDisabled)
-        {
-            GetComponent<SpriteRenderer>().material = minigame.GetDefaultMaterial(); 
-        }
+            NotPressed(); 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "PlayerAttack")
         {
-            minigame.ButtonInteraction(scoreValue, ballNumber, GetComponent<SpriteRenderer>());
+            minigame.ButtonInteraction(scoreValue, ballNumber, this);
         }
+    }
+
+    public void Pressed()
+    {
+        myRenderer.material = toggleMaterial;
+        myRenderer.sprite = toggledSprite; 
+    }
+
+    public void NotPressed()
+    {
+        myRenderer.material = defaultMaterial;
+        myRenderer.sprite = defaultSprite; 
     }
 }
