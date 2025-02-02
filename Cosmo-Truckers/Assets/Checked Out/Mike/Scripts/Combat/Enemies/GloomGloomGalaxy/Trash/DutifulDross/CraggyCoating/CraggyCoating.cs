c7f5a121d2d21ec9fb3cfc41e4a60f268ac59b14
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2025-02-02 17:55:20 -0500

    holy fucking shit

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Trash/DutifulDross/CraggyCoating/CraggyCoating.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Trash/DutifulDross/CraggyCoating/CraggyCoating.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class CraggyCoating : CombatMove
 {
-    [SerializeField] float maxTimeSuccess;
+    [SerializeField] float timeBetweenRocks = 1f;
+    [SerializeField] CraggyCoatingEnemy[] rocks;
+
+    int currentRock = 0;
 
     private void Start()
     {
-        GenerateLayout();
+        foreach (CraggyCoatingEnemy rock in rocks)
+            rock.enabled = false;
     }
+
+    public override void StartMove()
+    {
+        base.StartMove();
+        StartCoroutine(RockStarter());
+    }
+
+    IEnumerator RockStarter()
+    {
+        yield return new WaitForSeconds(timeBetweenRocks);
+
+        rocks[currentRock].enabled = true;
+        currentRock++;
+
+        if(currentRock < rocks.Length)
+            StartCoroutine(RockStarter());
+    }
+
 
     public override void EndMove()
     {
--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Trash/DutifulDross/CraggyCoating/CraggyCoatingEnemy.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/Combat/Enemies/GloomGloomGalaxy/Trash/DutifulDross/CraggyCoating/CraggyCoatingEnemy.cs
@@ -8,12 +8,16 @@ public class CraggyCoatingEnemy : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] ParticleSystem starTrail;
     [SerializeField] float waitToStartTrail = 1.25f;
+    [SerializeField] float spawnRangeY = 4.25f;
+    [SerializeField] float spawnRangeX = 6.5f;
     float timer;
 
     CraggyCoating minigame;
 
     private void Start()
     {
+        RandomizeStartPosition();
+
         transform.right = target.position - transform.position;
         minigame = FindObjectOfType<CraggyCoating>();
     }
@@ -46,6 +50,37 @@ public class CraggyCoatingEnemy : MonoBehaviour
             UnchildParticle(); 
             Destroy(gameObject);
         }
+    }
+
+    private void RandomizeStartPosition()
+    {
+        int quadrant = Random.Range(0, 4);
+        float randomY = 0;
+        float randomX = 0;
+
+        switch (quadrant)
+        {
+            case 0:
+                randomY = Random.Range(-spawnRangeY, spawnRangeY);
+                randomX = -spawnRangeX;
+                break;
+            case 1:
+                randomY = Random.Range(-spawnRangeY, spawnRangeY);
+                randomX = spawnRangeX;
+                break;
+            case 2:
+                randomY = -spawnRangeY;
+                randomX = Random.Range(-spawnRangeX, spawnRangeX);
+                break;
+            case 3:
+                randomY = spawnRangeY;
+                randomX = Random.Range(-spawnRangeX, spawnRangeX);
+                break;
+            default:
+                break;
+        }
+
+        transform.localPosition = new Vector3(randomX, randomY, transform.localPosition.z);
     }
 
     void UnchildParticle()
