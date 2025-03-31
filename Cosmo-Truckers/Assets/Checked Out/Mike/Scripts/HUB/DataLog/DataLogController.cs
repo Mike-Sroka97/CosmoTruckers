5354b25263af4a0e49f9c4f60e6c8ba011ece74a
Mike-Sroka97 <86737337+Mike-Sroka97@users.noreply.github.com>
2025-03-30 20:32:27 -0400

    ignore the last commit message

--- Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/HUB/DataLog/DataLogController.cs
+++ Cosmo-Truckers/Assets/Checked Out/Mike/Scripts/HUB/DataLog/DataLogController.cs
@@ -12,10 +12,16 @@ public class DataLogController : MonoBehaviour
     [Header("Secondary Screens")]
     [SerializeField] GameObject secondaryScreen;
     [SerializeField] GameObject[] secondaryScreens;
+    [SerializeField] GameObject[] npcScreens;
+    [SerializeField] GameObject[] enemiesScreens;
+    [SerializeField] GameObject[] enemySummonsScreens;
+    [SerializeField] GameObject[] friendlySummonsScreens;
 
     [Space(20)]
     [Header("Yap")]
     public TextMeshProUGUI DataYapAura;
+
+    [HideInInspector] public int currentDimension;
 
     /// <summary>
     /// On enable sets default state of the data log
@@ -47,9 +53,43 @@ public class DataLogController : MonoBehaviour
         DataYapAura.text = "";
     }
 
+    /// <summary>
+    /// Sets a derivative screen of the main screen
+    /// </summary>
+    /// <param name="screenToSet"></param>
     public void SetSecondaryScreen(GameObject screenToSet)
     {
         SetSecondaryScreens();
         screenToSet.SetActive(true);
+    }
+
+    /// <summary>
+    /// Sets single dimension data screen to be DRY
+    /// </summary>
+    /// <param name="screenToSet"></param>
+    /// <param name="dimension"></param>
+    public void SetDungeonScreen(int dimension)
+    {
+        currentDimension = dimension;
+    }
+
+    public void SetNpcScreen()
+    {
+        SetSecondaryScreen(npcScreens[currentDimension - 1]);
+    }
+
+    public void SetEnemyScreen()
+    {
+        SetSecondaryScreen(enemiesScreens[currentDimension - 1]);
+    }
+
+    public void SetEnemySummonScreen()
+    {
+        SetSecondaryScreen(enemySummonsScreens[currentDimension - 1]);
+    }
+
+    public void SetFriendlySummonScreen()
+    {
+        SetSecondaryScreen(friendlySummonsScreens[currentDimension - 1]);
     }
 }
